f2037ad158a12bb63bff037b07f36cf343d73d4e
Zdekh Kirill <zdekhKirill@mail.ru>
2025-11-13 22:18:50 +0300

    start_commit

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Edge {
+    int u, v, w;
+};
+
+int parent[100005];
+
+int find(int x) {
+    if (parent[x] != x)
+        parent[x] = find(parent[x]);
+    return parent[x];
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<Edge> edges(m);
+    for (int i = 0; i < m; i++) {
+        cin >> edges[i].u >> edges[i].v >> edges[i].w;
+    }
+
+    // Сортировка рёбер по весу
+    sort(edges.begin(), edges.end(), [](Edge a, Edge b) {
+        return a.w < b.w;
+    });
+
+    // Инициализация DSU
+    for (int i = 0; i < n; i++) parent[i] = i;
+
+    int totalWeight = 0;
+    for (auto e : edges) {
+        int rootU = find(e.u);
+        int rootV = find(e.v);
+        if (rootU != rootV) {
+            parent[rootU] = rootV;
+            totalWeight += e.w;
+        }
+    }
+
+    cout << "Вес минимального остовного дерева: " << totalWeight << endl;
+
+    return 0;
+}
